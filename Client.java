3cf9d8bfbe0d0f61e8a91726ba0848b7d50cf672
aleyii <ahkrayem@hotmail.com>
2018-01-28 15:10:19 -0500

    Add files via upload

--- Client.java
+++ Client.java
@@ -4,77 +4,77 @@
 
 import java.io.*;
 import java.net.*;
-
+import java.util.*;
 public class Client 
 {
-    public static final int SERVER_PORT = 3246;
+    public static final int SERVER_PORT = 3245;
 
     public static void main(String[] args) 
     {
-	Socket clientSocket = null;  
-	PrintStream os = null;
-	BufferedReader is = null;
-	String userInput = null;
-	String serverInput = null;
-	BufferedReader stdInput = null;
-
-	//Check the number of command line parameters
-	if (args.length < 1)
-	{
-	    System.out.println("Usage: client <Server IP Address>");
-	    System.exit(1);
-	}
-
-	// Try to open a socket on SERVER_PORT
-	// Try to open input and output streams
-	System.out.println("Client online");
-	try 
-	{
-	    clientSocket = new Socket(args[0], SERVER_PORT);
-	    os = new PrintStream(clientSocket.getOutputStream());
-	    is = new BufferedReader (
-		new InputStreamReader(clientSocket.getInputStream()));
-	    stdInput = new BufferedReader(new InputStreamReader(System.in));
-	} 
-	catch (UnknownHostException e) 
-	{
-	    System.err.println("Don't know about host: hostname");
-	} 
-	catch (IOException e) 
-	{
-	    System.err.println("Couldn't get I/O for the connection to: hostname");
-	}
-
-	// If everything has been initialized then we want to write some data
-	// to the socket we have opened a connection to on port 25
-
-	if (clientSocket != null && os != null && is != null) 
-	{
-	    try 
-	    {
-		while ((userInput = stdInput.readLine())!= "end")
+		Socket clientSocket = null;  
+		PrintStream os = null;
+		BufferedReader is = null;
+		String userInput = null;
+		String serverInput = null;
+		BufferedReader stdInput = null;
+	
+		//Check the number of command line parameters
+		if (args.length < 1)
 		{
-		    os.println(userInput);
-		    serverInput = is.readLine();
-		    System.out.println("Echo:" + serverInput);
-		    if (serverInput.equals("QUIT"))
-		    		break;
-		    else if(serverInput.equals("SHUTDOWN"))
-		    		break;
+		    System.out.println("Usage: client <Server IP Address>");
+		    System.exit(1);
 		}
-
-		// close the input and output stream
-		// close the sockset
-
-		os.close();
-		is.close();
-		clientSocket.close();   
-		System.out.println("client closed");
-	    } 
-	    catch (IOException e) 
-	    {
-		System.err.println("IOException:  " + e);
-	    }
-	}
+	
+		// Try to open a socket on SERVER_PORT
+		// Try to open input and output streams
+		System.out.println("Client online");
+		try 
+		{
+		    clientSocket = new Socket(args[0], SERVER_PORT);
+		    os = new PrintStream(clientSocket.getOutputStream());
+		    is = new BufferedReader (
+			new InputStreamReader(clientSocket.getInputStream()));
+		    stdInput = new BufferedReader(new InputStreamReader(System.in));
+		} 
+		catch (UnknownHostException e) 
+		{
+		    System.err.println("Don't know about host: hostname");
+		} 
+		catch (IOException e) 
+		{
+		    System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+	
+		// If everything has been initialized then we want to write some data
+		// to the socket we have opened a connection to on port 25
+	
+		if (clientSocket != null && os != null && is != null) 
+		{
+		    try 
+		    {
+				while ((userInput = stdInput.readLine())!= null)
+				{
+				    os.println(userInput);
+				    serverInput = is.readLine();
+				    System.out.println(serverInput);
+				   
+				     if(serverInput.equals("SHUTDOWN")||serverInput.equals("QUIT"))
+				    		break;
+				}
+		
+				// close the input and output stream
+				// close the sockset
+		
+				os.close();
+				is.close();
+				clientSocket.close();   
+				System.out.println("client closed");
+				System.exit(0);
+		    } 
+		    catch (IOException e) 
+		    {
+		    		System.err.println("IOException:  " + e);
+		    }
+		}
     }           
 }
--- Server.java
+++ Server.java
@@ -9,91 +9,83 @@ import java.util.Scanner;
 import java.util.*;
 
 public class Server {
-
-    public static final int SERVER_PORT = 3246;
+    public static final int SERVER_PORT = 3245;
 
     public static void main(String args[]) 
     {
-	ServerSocket myServerice = null;
-	String line;
-	BufferedReader is;
-	PrintStream os;
-	Socket serviceSocket = null;
-
-	// Try to open a server socket 
-	try {
-	    myServerice = new ServerSocket(SERVER_PORT);
-	    System.out.println("Connected");
-	}
-	catch (IOException e) {
-	    System.out.println(e);
-	    System.out.println("Connection failed");
-	}   
-
-	// Create a socket object from the ServerSocket to listen and accept connections.
-	// Open input and output streams
-
-	while (true)
-	{
-	    try 
-	    {
-		serviceSocket = myServerice.accept();
-		is = new BufferedReader (new InputStreamReader(serviceSocket.getInputStream()));
-		os = new PrintStream(serviceSocket.getOutputStream());
-
-		// Open the file holding data
-		// move data into the arraylist
-		
-		// As long as we receive data, echo that data back to the client.
-		ArrayList<ArrayList<String>> contacts = new ArrayList<ArrayList<String>>();
-		 final int max = 20;
-		// fileimport(contacts);
-		while ((line = is.readLine()) != null) 
-		{	
-			//ADD
-			if (line.substring(0, 3).equals("ADD"))
-				System.out.println("function" + line); // REMOVE ADD FROM STRING AND PASS TO FUNCTION
-			// Delete
-			else  if (line.substring(0, 4).equals("LIST"))
-				System.out.println("function" + line);
-			// LIST
-			else if (line.substring(0, 4).equals("QUIT"))
-				System.out.println("function" + line);
-			// QUIT
-			else if (line.substring(0, 6).equals("DELETE"))
-				System.out.println("function" + line);
-				// closes the clients connection
-			//SHUTDOWN
-			else if (line.substring(0, 8).equals("SHUTDOWN")) {
-				System.out.println("function" + line);
-				break;
-				
-			}
-				// write the data of the array lists to the file 
-		   
-		    os.println(line); 
+		ServerSocket myServerice = null;
+		String line;
+		BufferedReader is;
+		PrintStream os;
+		Socket serviceSocket = null;
+	
+		// Try to open a server socket 
+		try {
+		    myServerice = new ServerSocket(SERVER_PORT);
+		    System.out.println("Connected");
 		}
-
-		//close input and output stream and socket
-		is.close();
-		os.close();
-		serviceSocket.close();
-		System.out.println("Server DOWN");
-		System.exit(0);
-	    }   
-	    catch (IOException e) 
-	    {
-		System.out.println(e);
-	    }
-	}
+		catch (IOException e) {
+		    System.out.println(e);
+		    System.out.println("Connection failed");
+		}   
+	
+		// Create a socket object from the ServerSocket to listen and accept connections.
+		// Open input and output streams
+	
+		while (true)
+		{
+		    try 
+		    {
+				serviceSocket = myServerice.accept();
+				is = new BufferedReader (new InputStreamReader(serviceSocket.getInputStream()));
+				os = new PrintStream(serviceSocket.getOutputStream());
+		
+				// Open the file holding data
+				// move data into the arraylist
+				
+				ArrayList<ArrayList<String>> contacts = new ArrayList<ArrayList<String>>();
+				 final int max = 20;
+				// fileimport(contacts);
+				while ((line = is.readLine()) != null) 
+				{	
+					//ADD
+					if (line.substring(0, 3).equals("ADD")) 
+						add(line,contacts,os,max);
+					else  if (line.substring(0, 4).equals("LIST"))
+						list (contacts,os);
+					else if (line.substring(0, 4).equals("QUIT"))
+						os.println(line);
+					else if (line.substring(0, 6).equals("DELETE")) 
+						delete(contacts, line, os);
+					else if (line.substring(0, 8).equals("SHUTDOWN")) {
+						 os.println("200 OK"); 
+						break;
+					}
+					// write the data of the array lists to the file 
+				   
+				    os.println("200 OK"); 
+				}
+		
+				//close input and output stream and socket
+				is.close();
+				os.close();
+				serviceSocket.close();
+				System.out.println("Server DOWN");
+				System.exit(0);
+		    }   
+		    catch (IOException e) 
+		    {
+		    		System.out.println(e);
+		    }
+		}
     }
     
-
     private Scanner x;
 	public void openFile()
 	{
 		try{
 			x = new Scanner (new File("conatcts.txt"));
+			System.out.println("File Opened");
 			
 		}
 		catch(Exception e){
@@ -106,66 +98,54 @@ public class Server {
 	{
 		int rows=0;
 		while(x.hasNext())
-		{
 			rows++;
-		}
-		
 		
 		x.close();
-		
 	}
 	
-	
-	void add(String line,ArrayList<ArrayList<String>> contacts) {
+	static void add(String line,ArrayList<ArrayList<String>> contacts,PrintStream os, int max) {
 		//if(spotOpen(contacts))
 		// insert data
 	    //else
-		
+		if (contacts.size()<max) {
 		String[] parts = line.split(" ");
-		//create the id --> push to first column
-			// contracts.size()+1001
 		contacts.add(new ArrayList<String>());
 		contacts.get(contacts.size()-1).add(Integer.toString(contacts.size()+1000)); // ID generated
-		contacts.get(contacts.size()-1).add(parts[0]); // ADD FNAME 
-		contacts.get(contacts.size()-1).add(parts[1]); // ADD LNAME
-		if(phonePattern(parts[2])) // ADD PHONE NUMBER 
-			contacts.get(contacts.size()-1).add(parts[2]);
+		contacts.get(contacts.size()-1).add(parts[1]); // ADD FNAME 
+		contacts.get(contacts.size()-1).add(parts[2]); // ADD LNAME
+		
+		if(parts[3].matches("(\\d-)?(\\d{3}-)?\\d{3}-\\d{4}")) // check if phone follows format
+			contacts.get(contacts.size()-1).add(parts[3]);  // if follows format, insert into array
 		else
 			System.out.println("Throw exception");
+		
 		// if exception thrown delete the data from that row 
-		
-		// return code 200 ok
+		}
 	}
 	
-	void delete(ArrayList<ArrayList<String>> contacts, String ID) {
-		int result = Integer.parseInt(ID);
-		System.out.println(result);
+	static void delete(ArrayList<ArrayList<String>> contacts, String line,PrintStream os) {
+	
+		String[] parts = line.split(" ");
+		int result = Integer.parseInt(parts[1]);
+				
 		result -= 1001;
-		
 		contacts.get(result).clear();
-		//return code 200 ok
-	}
+		os.println("200 OK"); 
+	}// DONE
 	
-	void list(ArrayList<ArrayList<String>> contacts,PrintStream os){
-		for(int i=0;i< contacts.size();i++)
-			for(int j=0;j<contacts.get(i).size();j++)
-				os.println(j + " \t");
-	}
+	static void list(ArrayList<ArrayList<String>> contacts,PrintStream os){		
+		for(int i=0;i< contacts.size();i++) {
+			for(int j=0;j<contacts.get(i).size();j++) {
+				System.out.print(contacts.get(i).get(j) + " \t");
+			}
+			System.out.println();
+		}
+	} 
 
 	boolean spotOpen(ArrayList<ArrayList<String>> contacts) {
 		for(int i=0;i<contacts.size();i++)
-			if(contacts.get(i).get(0)!= null)
+			if(contacts.get(i).get(0)== null)
 				return true;
-		return false;
-	}
-	boolean phonePattern(String phone) {
-		
-		boolean match = false;
-	      String phoneNumberPattern = "(\\d-)?(\\d{3}-)?\\d{3}-\\d{4}";
-	      match = phone.matches(phoneNumberPattern);
-	      if(match)
-	    	  	return true;
-		
 		return false;
 	}
 }
